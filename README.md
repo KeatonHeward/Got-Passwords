404cc16da770f918a9d718cedb58ed659489db33
Keaton <keat1232@gmail.com>
2022-07-25 13:02:29 -0600

    changed user inputs and got help from tutor

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Got-Passwords
+things to do.
+generate a password when clicking the button.
+prompt user for password criteria. (8-128 characters, uppercase, lowercase, numeric, and or special characters.)
+generate password that meets the chosen criteria.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,53 @@
 // Assignment code here
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowers = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specials = "!#%&'()*+,-./:;<=>?@]^_`{|}~]";
+var user = "";
+var random = "";
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function writePassword() {
+
+  var pLength = window.prompt("How long would you like your password? 8-128 characters.")
+  if (pLength <= 7 || pLength >= 129) {
+    alert("password must be within the parameters");
+    return;
+  };
+
+  var letter1 = window.confirm("Would you like upper case letters?");
+  if (letter1) {
+    user+=upper
+  };
+
+  var letter2 = window.confirm("Would you like lower case letters?");
+  if (letter2) {
+    user+=lowers
+  };
+
+  var number1 = window.confirm("Would you like numbers?");
+  if (number1) {
+    user+=numbers
+  };
+
+  var special = window.confirm("Would you like speical characters?");
+  if (special) {
+    user+=specials
+  };
+  
+  for (var i=0; i<pLength; i++) {
+    random += user.charAt(Math.floor(Math.random() * user.length))
+  }
+  
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password.join("");
+
+};
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
